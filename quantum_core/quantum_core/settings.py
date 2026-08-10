faa3b52bb126c5a9dd45be529c2ed9172e9881f3
Nitish Kumar <nitishkumar07689@gmail.com>
2026-08-11 02:01:37 +0530

    Fixed CORS spelling error

--- quantum_core/quantum_core/settings.py
+++ quantum_core/quantum_core/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     "https://localhost:5173",
     "https://cyber-zen.vercel.app",
 ]
